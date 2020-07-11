05362cb7aa20e0a8331432de18b7f3929ab7e48b
jazielg <jazielgj1995@gmail.com>
2020-07-10 21:26:57 -0300

    37 - more flexbox styling

--- goals-app/components/GoalInput.js
+++ goals-app/components/GoalInput.js
@@ -25,8 +25,8 @@ const GoalInput = (props) => {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
   },
   input: {
@@ -34,6 +34,7 @@ const styles = StyleSheet.create({
     borderColor: "black",
     borderWidth: 1,
     padding: 10,
+    marginBottom: 10,
   },
 });
 
