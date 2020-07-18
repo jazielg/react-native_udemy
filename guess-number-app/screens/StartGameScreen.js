49463112cce0aa720444db27c4180e2e907b39bf
jazielg <jazielgj1995@gmail.com>
2020-07-17 22:00:11 -0300

    57 - styling a view as a card container (with drop shadows and rounded corners)

--- guess-number-app/screens/StartGameScreen.js
+++ guess-number-app/screens/StartGameScreen.js
@@ -31,6 +31,14 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8, // shadow on android
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
