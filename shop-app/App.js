a4424f3e7ca9328e785e5e06c11328c1f726d9ca
jazielg <jazielgj1995@gmail.com>
2021-02-17 11:53:30 -0300

    165 - the products overview screen

--- shop-app/App.js
+++ shop-app/App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View } from "react-native";
 
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import ProductsReducer from "./store/reducers/products";
+
+const rootReducer = combineReducers({
+  products: ProductsReducer,
+});
+
+const store = createStore(rootReducer);
+
 export default function App() {
   return (
-    <View>
-      <Text>A new app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View>...</View>
+    </Provider>
   );
 }
