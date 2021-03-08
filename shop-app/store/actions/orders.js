1e454751b02e7f1950e13c5aae317bb4c0063056
jazielg <jazielgj1995@gmail.com>
2021-03-08 20:15:20 -0300

    211 - fetching stored orders

--- shop-app/store/actions/orders.js
+++ shop-app/store/actions/orders.js
@@ -1,4 +1,39 @@
+import Order from "../../models/order";
+
 export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDERS = "SET_ORDERS";
+
+export const fetchOrders = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        "https://udemy-react-native-shop-app-default-rtdb.firebaseio.com/orders/u1.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const resData = await response.json();
+      const loadOrders = [];
+
+      for (const key in resData) {
+        loadOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+
+      dispatch({ type: SET_ORDERS, orders: loadOrders });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch) => {
