495d372266cda6c474c81d51a2145cc8f49e0db8
jazielg <jazielgj1995@gmail.com>
2021-03-20 12:11:01 -0300

    223 - mapping orders to users

--- shop-app/store/actions/products.js
+++ shop-app/store/actions/products.js
@@ -6,8 +6,9 @@ export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const SET_PRODUCTS = "SET_PRODUCTS";
 
 export const fetchProducts = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     // any async code you want!
+    const userId = getState().auth.userId;
     try {
       const response = await fetch(
         "https://udemy-react-native-shop-app-default-rtdb.firebaseio.com/products.json"
@@ -36,6 +37,7 @@ export const fetchProducts = () => {
       dispatch({
         type: SET_PRODUCTS,
         products: loadedProducts,
+        userProducts: loadedProducts.filter((prod) => prod.ownerId === userId),
       });
     } catch (err) {
       // send to custom analytics server
@@ -66,6 +68,7 @@ export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
     // any async code you want!
     const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const response = await fetch(
       `https://udemy-react-native-shop-app-default-rtdb.firebaseio.com/products.json?auth=${token}`,
       {
@@ -73,7 +76,13 @@ export const createProduct = (title, description, imageUrl, price) => {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ title, description, imageUrl, price }),
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+          price,
+          ownerId: userId,
+        }),
       }
     );
 
@@ -87,6 +96,7 @@ export const createProduct = (title, description, imageUrl, price) => {
         description,
         imageUrl,
         price,
+        ownerId: userId,
       },
     });
   };
