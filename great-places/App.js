7fd4906d13d413a5416c78b36627c40148cfd889
jazielg <jazielgj1995@gmail.com>
2021-03-21 16:11:25 -0300

    234 - screen and navigation setup

---
+++ great-places/App.js
@@ -0,0 +1,13 @@
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+import PlacesNavigator from "./navigation/PlacesNavigation";
+
+export default function App() {
+  return (
+    <>
+      <PlacesNavigator />
+      {/* <StatusBar style="auto" /> */}
+    </>
+  );
+}
