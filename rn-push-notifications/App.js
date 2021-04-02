767e19f4b534c4eca7aa61c7412c9e4c8d467e7c
jazielg <jazielgj1995@gmail.com>
2021-04-02 10:59:07 -0300

    299 - sending local notifications

---
+++ rn-push-notifications/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+export default function App() {
+  const triggerNotificationHandler = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first local notification",
+        body: "This is the first local notification we are sending!",
+      },
+      trigger: {
+        seconds: 10,
+      },
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Trigger Notification"
+        onPress={triggerNotificationHandler}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
