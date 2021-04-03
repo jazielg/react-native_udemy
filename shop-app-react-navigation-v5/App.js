a936876f090f4c4a7e0d2941a437495e7e5774ec
jazielg <jazielgj1995@gmail.com>
2021-04-03 15:03:13 -0300

    310 - adding push notifications to the shop app (3/3)

--- shop-app-react-navigation-v5/App.js
+++ shop-app-react-navigation-v5/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import ReduxThunk from "redux-thunk";
+import * as Notifications from "expo-notifications";
 // import { composeWithDevTools } from "redux-devtools-extension";
 
 import AppNavigator from "./navigation/AppNavigator";
@@ -12,6 +13,14 @@ import cartReducer from "./store/reducers/cart";
 import productsReducer from "./store/reducers/products";
 import ordersReducer from "./store/reducers/orders";
 import authReducer from "./store/reducers/auth";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
 
 const rootReducer = combineReducers({
   products: productsReducer,
